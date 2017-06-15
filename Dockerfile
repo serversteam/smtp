1918816bb232370abc47a7c1f9e121b0eb669680
serverteam <365.orion@gmail.com>
2017-06-16 00:40:40 +0530

    smtp

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,8 @@ WORKDIR /etc/mail
 
 RUN set -e && \
  apt-get update && \
- apt-get install -y sendmail
+ apt-get install -y sendmail && \
+ apt-get install mailutils -y
 
 COPY sendmail.mc /etc/mail/sendmail.mc
 
@@ -14,7 +15,10 @@ RUN m4 sendmail.mc > sendmail.cf && \
  echo "Connect:172 RELAY" >> access && \
  echo "Connect:10 RELAY" >> access && \
  make
-
 EXPOSE 25
 
 CMD /usr/lib/sendmail -bD -X /proc/self/fd/1
+
+COPY app/ /app/
+RUN chmod +x /app/init.sh
+ENTRYPOINT ["/app/init.sh"]
---
+++ app/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+adduser clood_smtp --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "clood_smtp:EiBohsh9aeNge4ieyooSh8Xe" | chpasswd
+
+adduser nagios_test --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "nagios_test:queZ2eadchei5Ohj" | chpasswd
+
+adduser sertone --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "sertone:Eed6quaeboh8aiQuneeGu7ah" | chpasswd
+
+#useradd -ms /bin/bash clood_smtp
+#echo "clood_smtp:EiBohsh9aeNge4ieyooSh8Xe" | /usr/sbin/chpasswd
+service sendmail start 
+
+#useradd -ms /bin/bash nagios_test
+#echo "nagios_test:queZ2eadchei5Ohj" | /usr/sbin/chpasswd
+
+#useradd -ms /bin/bash sertone
+#echo "sertone:Eed6quaeboh8aiQuneeGu7ah" | /usr/sbin/chpasswd
+/bin/bash
+
+#touch /var/log/syslog
+#tailf /var/log/syslog
